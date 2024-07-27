7391babefca4d4ce1bc719ce53535736f02cadc0
Martin Young <MartingYoung@hotmail.com>
2024-07-27 14:46:26 -0500

    trying to get the #previewImageModal image-description to accept CSS

--- scripts/index.js
+++ scripts/index.js
@@ -30,6 +30,7 @@ const cardTemplate = document
   .querySelector("#card-template")
   .content.querySelector(".card");
 
+  // cards data for a list
 const cardsWrap = document.querySelector(".cards__list");
 
 /*-------------------------------------------------------------------*/
@@ -40,12 +41,15 @@ const profileEditButton = document.querySelector(".profile__edit-button");
 //instantiate the FULL modal for profile and card
 const editProfileModal = document.querySelector("#edit-modal");
 const addCardModal = document.querySelector("#add-card-modal");
+const previewImageModal = document.querySelector("#preview-image-modal");
 //use the above variable to instantiate the modal__form
 const profileFormElement = editProfileModal.querySelector(".modal__form");
 const addCardFormElement = addCardModal.querySelector(".modal__form");
+
 // use the FULL model to find modal__close button
 const profileModalCloseButton = editProfileModal.querySelector(".modal__close");
-const addModalCloseButton = addCardModal.querySelector(".modal__close");
+const addCardModalCloseButton = addCardModal.querySelector(".modal__close");
+const previewImageModalCloseButton = previewImageModal.querySelector(".modal__close");
 
 //profile data
 const profileTitle = document.querySelector(".profile__title");
@@ -62,8 +66,8 @@ const cardUrlInput = addCardFormElement.querySelector(".modal__input_type_url");
 // button to create a new card
 const addNewCardButton = document.querySelector(".profile__add-button");
 
-// cards data for a list
-const cardListEl = document.querySelector(".cards__list");
+
+
 
 /*-------------------------------------------------------------------*/
 /*                         Functions                                 */
@@ -72,16 +76,14 @@ function closeModal(modal) {
   modal.classList.remove("modal_opened");
 }
 
-/* function openModal() {
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileDescription.textContent;
-
-  editProfileModal.classList.add("modal_opened");
-} */
-
 function openModal(modal) {
   modal.classList.add("modal_opened");
 }
+
+function renderCard(cardData, wrapper) {
+  const cardElement = getCardElement(cardData);
+  wrapper.prepend(cardElement);
+};
 
 function handleProfileFormSubmit(e) {
   e.preventDefault();
@@ -92,26 +94,62 @@ function handleProfileFormSubmit(e) {
 
 function handleAddCardFormSubmit(e) {
   e.preventDefault();
-  const cardElement = getCardElement();
+  const name = cardTitleInput.value;
+  const link = cardUrlInput.value;
+  renderCard({ name, link }, cardsWrap);
   closeModal(addCardModal);
 }
 
-function getCardElement(cardData) {
+function getCardElement(data) {
   const cardElement = cardTemplate.cloneNode(true);
-  const cardImageEl = cardElement.querySelector(".card__image");
-  const cardTitleEl = cardElement.querySelector(".card__title");
-  cardImageEl.src = cardData.link;
-  cardImageEl.alt = cardData.name;
-  cardTitleEl.textContent = cardData.name;
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  
+  
+  //use cardImage. Add click event listener to the ccardImage 
+    //openModel() with previewImageModal. Add previewImageModal to HTML.
+    //go to the opened image source and change it to the cardImage source.
+    //...we've done this multiple times.
+  
+    //next task: smooth in effect. Read the article.  14:30 
+  
+    cardImage.addEventListener("click", () => {
+      openModal(previewImageModal);
+      const modalImage = previewImageModal.querySelector("#card__image");
+      const imageDescription = previewImageModal.querySelector("#image-description");
+      modalImage.src = data.link;
+      modalImage.alt = data.name;
+      imageDescription.textContent = modalImage.alt;
+    });
+
+    // add event listener
+  // add cardElement.remove();
+  deleteButton.addEventListener("click", () => {
+    deleteButton.classList.toggle('card__delete-button_active');
+    cardElement.remove();
+  });
+
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle('card__like-button_active');
+  });
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
 
   return cardElement;
 }
+
+
 
 /*-------------------------------------------------------------------*/
 /*                      Event Listeners                              */
 /*-------------------------------------------------------------------*/
 
 profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+addCardFormElement.addEventListener("submit", handleAddCardFormSubmit);
+
 profileEditButton.addEventListener("click", () => {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileDescription.textContent;
@@ -122,8 +160,9 @@ profileModalCloseButton.addEventListener("click", () =>
 );
 // add new card
 addNewCardButton.addEventListener("click", () => openModal(addCardModal));
-addModalCloseButton.addEventListener("click", () => closeModal(addCardModal));
+addCardModalCloseButton.addEventListener("click", () => closeModal(addCardModal));
+previewImageModalCloseButton.addEventListener("click", () => closeModal(previewImageModal));
 
-initialCards.forEach((cardData) => {
-  cardsWrap.prepend(getCardElement(cardData));
-});
+initialCards.forEach((cardData) => renderCard(cardData, cardsWrap));
+
+
