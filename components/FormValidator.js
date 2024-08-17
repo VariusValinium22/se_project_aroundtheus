97ec719bc8e4703470a4d84542ca74c3b1dad9de
Martin Young <MartingYoung@hotmail.com>
2024-08-17 11:36:11 -0500

    how do I conceive of the FormValidator class/module?

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -2,4 +2,27 @@ export default class FormValidator {
     constructor() {
 
     }
+    enableValidation() {
+
+    }
+
 }
+
+// when form fields are invalid : submission button remains disabled
+//                                              retain current data
+//  edit profile modal form AFTER submission : retain new data 
+//                                              submission button disabled.
+//              (currently is not disabled when reopened after submission)
+//when forms submitted : clear form/submission button disabled
+
+//when form is closed/unsubmitted: retain data
+
+//Resetting validation [optional]
+//AFTER submitting the card form, 
+//          reset the validation error messages on the card modal
+//          when resetting the form fields and disabling the submit button. 
+
+//for the edit profile form
+//          when opening the form since the form fields are vaild
+
+
